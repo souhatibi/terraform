b3772ae8ccb47afc6e3450f03e966b3e135e826c
Souha Tibi <souhatibi@gmail.com>
2023-11-11 22:59:53 +0100

    Deploy instance with condition

---
+++ demo07/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_instance" "app_server" {
+  count         = var.create_instance ? 1 : 0
+  ami           = "ami-0c95ddc49a2ac351f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "app-server-instance"
+  }
+}
---
+++ demo07/variables.tf
@@ -0,0 +1,5 @@
+variable "create_instance" {
+    description = "Boolean that determines if an ec2 instance should be created"
+    type        = bool
+    default     = false
+}
