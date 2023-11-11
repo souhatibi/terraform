7b4eef02b0d7a6a4976c7f66d303fdff305ffebe
Souha Tibi <souhatibi@gmail.com>
2023-11-11 11:25:09 +0100

    Deploy vpc

---
+++ demo02/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
